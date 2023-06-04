8d510583d0d5f10aa1697bbb8de3d479d233250d
timcod067 <anrsohandsome@gmail.com>
2023-06-04 22:54:44 +0800

    change_path

--- app.py
+++ app.py
@@ -4,8 +4,24 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def firstpage():  # put application's code here
     return render_template('firstpage.html')
+
+@app.route('/checi')
+def checi():  # put application's code here
+    return render_template('checi.html')
+
+@app.route('/money')
+def money():  # put application's code here
+    return render_template('money.html')
+
+@app.route('/outlink')
+def outlink():  # put application's code here
+    return render_template('page4.html')
+
+@app.route('/aboutus')
+def aboutus():  # put application's code here
+    return render_template('finalpage.html')
 
 if __name__ == '__main__':
     app.run()
