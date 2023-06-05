8ec337cd18f2fc04326445bab0ca4a8cba7254f1
a20688392 <a20688392@gmail.com>
2023-06-05 21:54:10 +0800

    session cookie

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, redirect, session, url_for
+# from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 def firstpage():  # put application's code here
@@ -23,5 +25,41 @@ def outlink():  # put application's code here
 def aboutus():  # put application's code here
     return render_template('finalpage.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # if request.method == 'POST':
+        # account = request.form["account"]
+        # user_password = user.check(account)
+        # password = request.form["password"]
+        # bcrypt = Bcrypt()
+        # check = bcrypt.check_password_hash(user_password[0], password)
+        # if check == True:
+            useData = ['timping', 1]
+            session['userData'] = useData
+            return redirect(url_for('getsession'))
+    # return render_template('login.html')
+
+@app.route('/login1', methods=['GET', 'POST'])
+def login1():
+    # if request.method == 'POST':
+        # account = request.form["account"]
+        # user_password = user.check(account)
+        # password = request.form["password"]
+        # bcrypt = Bcrypt()
+        # check = bcrypt.check_password_hash(user_password[0], password)
+        # if check == True:
+            useData = ['jone', 2]
+            session['userData'] = useData
+            return redirect(url_for('getsession'))
+    # return render_template('login.html')
+
+@app.route('/getsession')
+def getsession():
+    if 'userData' in session:
+        print (session['userData'])
+        return session['userData'][0]
+    return "Not logged in!"
+
 if __name__ == '__main__':
+    app.config['SECRET_KEY'] = '12389!)!834913*&*&*&*'
     app.run()
